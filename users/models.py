cfc26ff3be9da9a211ab25543b40c99ccdbe02f7
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-07-25 23:18:33 +0300

    added: custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, username, password, **kwargs):
+
+        if not email:
+            raise ValueError('User should have valid email address')
+        if not username:
+            raise ValueError('User should have valid username')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username.lower(), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, username, password, **kwargs):
+        user = self.create_user(email, username, password, **kwargs)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=64, unique=True, db_index=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_activity = models.DateTimeField(null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = AccountManager()
+
+    def __str__(self):
+        return str(self.email)
